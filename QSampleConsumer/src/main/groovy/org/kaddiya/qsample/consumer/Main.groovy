54cded9f7caf024d4979651bf596e593dab92db6
Ameya <ameya.kulkarni@weboniselab.com>
2017-05-31 23:56:32 +0530

    Optimised the imports

--- QSampleConsumer/src/main/groovy/org/kaddiya/qsample/consumer/Main.groovy
+++ QSampleConsumer/src/main/groovy/org/kaddiya/qsample/consumer/Main.groovy
@@ -1,7 +1,30 @@
 package org.kaddiya.qsample.consumer
 
-/**
- * Created by Webonise on 31/05/17.
- */
+import groovy.util.logging.Slf4j
+import org.kaddiya.QClient.common.BrokerConfig
+import org.kaddiya.QClient.consumer.internal.SimpleConsumer
+import org.kaddiya.QClient.consumer.internal.example.SomePayload
+import org.kaddiya.qsample.commons.SampleMessage
+
+@Slf4j
 class Main {
+    public static void main(String[] args) {
+        //due to the type erasure we to tell our abstract class on how to unmarshall the content to our type and then pass in a
+        //a closure that will do the transformation and perform the sideeffect of when we receive a message
+        //In this example we will just log out the value of the `SomePayload`
+        //We can return SomePayload and then curry it to another function for jazzy transformation
+        Closure<SampleMessage> marshallingAndCallBackClosure = { String rawContent ->
+            try {
+                //need to serialise m.content into this class
+                log.info(rawContent)
+            } catch (Exception e) {
+                log.error("could not marshall the json" + e.getMessage())
+                return null
+            }
+        }
+
+
+        SimpleConsumer bc = new SimpleConsumer<SomePayload>("1", new BrokerConfig("http", "localhost", 8080), Arrays.asList(""), marshallingAndCallBackClosure)
+        bc.consumeMesage();
+    }
 }
--- QSampleProducer/src/main/groovy/org/kaddiya/qsample/producer/Main.groovy
+++ QSampleProducer/src/main/groovy/org/kaddiya/qsample/producer/Main.groovy
@@ -1,7 +1,5 @@
 package org.kaddiya.qsample.producer
 
-/**
- * Created by Webonise on 31/05/17.
- */
 class Main {
+
 }
